dc8308e9bd446995127e377b4306ae4ded3120d8
mahanhamisi <mahan.hamisi@gmail.com>
2025-08-06 01:45:19 +0330

    server side

--- src/client/java/net/mysticcreations/offchunk/OffChunkClient.java
+++ src/client/java/net/mysticcreations/offchunk/OffChunkClient.java
@@ -1,23 +1,10 @@
 package net.mysticcreations.offchunk;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.mysticcreations.offchunk.gpu.ComputeShaderHelper;
-
-import java.nio.FloatBuffer;
 
 public class OffChunkClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		System.out.println("[OffChunk] Initializing compute shader...");
-
-		ComputeShaderHelper shader = new ComputeShaderHelper(256, 256, "noise.comp");
-		shader.dispatch();
-		FloatBuffer data = shader.readTextureData();
-		for (int i = 0; i < 5 * 4; i += 4) {
-			float r = data.get(i);
-			float g = data.get(i + 1);
-			float b = data.get(i + 2);
-			System.out.printf("Noise: %.3f %.3f %.3f%n", r, g, b);
-		}
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
